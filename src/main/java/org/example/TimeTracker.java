88d3a3b055eecda5af62f2651022f0d6bf711208
AnnaKchma <anna_kuchma09@ukr.net>
2023-08-09 09:41:49 +0200

    initial commit

---
+++ src/main/java/org/example/TimeTracker.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class TimeTracker {
+    public static void main(String[] args) {
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+        executorService.scheduleAtFixedRate(new TimeTask(), 0, 1, TimeUnit.SECONDS);
+    }
+}
+
+class TimeTask implements Runnable {
+    private final long startTime = System.currentTimeMillis();
+
+    @Override
+    public void run() {
+        long currentTime = System.currentTimeMillis();
+        long elapsedMillis = currentTime - startTime;
+
+        System.out.printf("Time passed: %.2f seconds%n", elapsedMillis / 1000.0);
+
+        if (elapsedMillis % 5000 == 0) {
+            long minutesPassed = elapsedMillis / (1000 * 60);
+            System.out.println(minutesPassed + " minutes passed");
+        }
+    }
+}
