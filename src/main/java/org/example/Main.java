600117337e3d2295f458211b4ef33d0652ec3490
AnnaKchma <anna_kuchma09@ukr.net>
2023-08-09 09:47:42 +0200

    initial commit made in Main class test for FizzBuzzMultithreaded

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,45 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n = 20; // Change n as needed
+
+        FizzBuzzMultithreaded fizzBuzz = new FizzBuzzMultithreaded(n);
+
+        Thread threadA = new Thread(() -> {
+            try {
+                fizzBuzz.fizz();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread threadB = new Thread(() -> {
+            try {
+                fizzBuzz.buzz();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread threadC = new Thread(() -> {
+            try {
+                fizzBuzz.fizzbuzz();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread threadD = new Thread(() -> {
+            try {
+                fizzBuzz.number();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
+        threadD.start();
     }
 }
